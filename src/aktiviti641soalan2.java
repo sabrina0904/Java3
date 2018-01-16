f57bc3dd213b6cdc10ca6330e4af33623ed46c00
user <user@DESKTOP-OAL422G>
2018-01-17 00:39:53 +0800

    Java3

---
+++ src/aktiviti641soalan2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+import java.util.Scanner;
+public class aktiviti641soalan2 {
+    public static void main (String[] args){
+        
+        int gajiasas=800;
+        double gaji;
+        double bonus;
+        int bilpelanggan;
+        
+        Scanner input= new Scanner (System.in);
+        System.out.print("Masukkan bilangan pelanggan yang diperolehi semasa: ");
+        bilpelanggan=input.nextInt();
+        
+        if (bilpelanggan <=0){
+             bonus=0;
+        
+        }else if (bilpelanggan <=10){
+            bonus=550;
+            
+        }else if (bilpelanggan <=50){
+            bonus=1400;
+            
+        }else if (bilpelanggan <=100){
+            bonus=2500;
+            
+        }else {
+            bonus=1050;
+            
+        }
+        gaji=gajiasas +bonus;
+        System.out.println("Gaji anda: RM" +gaji);
+        
+        }
+    }
+
